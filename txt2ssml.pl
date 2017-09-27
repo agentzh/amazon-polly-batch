686923f4341949c1a443e26b614bdbb792a26b71
Yichun Zhang (agentzh) <agentzh@gmail.com>
2017-09-26 18:21:23 -0700

    txt2ssml.pl: fixed the "sentence too long" error.

--- txt2ssml.pl
+++ txt2ssml.pl
@@ -7,6 +7,8 @@ use warnings;
 
 use Getopt::Std;
 
+sub process_sentence ($$);
+
 my %opts;
 getopts("s:", \%opts) or die "Usage: $0 [-s SPEED] TXT-FILE\n";
 
@@ -24,6 +26,8 @@ while (<>) {
 
     chomp;
 
+    #warn "line: $_";
+
     if (/^[A-Z][^.,!?]+[a-z]$/) {
         my $len = bytes::length $_;
         if ($size + $len > 1500) {
@@ -35,6 +39,7 @@ while (<>) {
 
         if ($len <= 50) {
             #warn "size: $size, len: $len";
+            #warn "found title: $_";
 
             s/&/&amp;/g;
             s/</&lt;/g;
@@ -54,57 +59,67 @@ while (<>) {
 
     my $first = 1;
     while (/(.*?[.?!;:]['")]*)/gc) {
-        my $sentence = $1;
-        #warn $sentence;
-        my $len = bytes::length $sentence;
+        process_sentence($1, \$first);
+    }
 
-        #warn "size: $size, len: $len";
-
-        if ($size + $len > 1500) {
-            $chunk .= $closing_tags;
-            push @chunks, $chunk;
-            $chunk = $init_tags;
-            $size = 0;
-        }
-
-        if ($first) {
-            if ($paragraphs > 1) {
-                $chunk .= '<break time="500ms"/>';
-            }
-            undef $first;
-        }
-
-        $sentence =~ s/\&/\&amp;/g;
-        $sentence =~ s/</\&lt;/g;
-        $sentence =~ s/>/\&gt;/g;
-        #$sentence =~ s{N`([A-Za-z][^`]*?[A-Za-z])`}{<emphasis level="strong">$1</emphasis>}smg;
-        $sentence =~ s{VB`([A-Za-z][^`]*?[A-Za-z])`}{<w role="amazon:VB">$1</w>}smg;
-
-        $sentence =~ s{(?<!the|and) (["'])(.*?)\1}{
-            my $mark = $1;
-            my $v = $2;
-            if ($v =~ /^\w+$/) {
-                qq! <emphasis>$v</emphasis>!
-            } else {
-                qq! <break time="250ms"/>$mark$v$mark!
-            }
-        }esmg;
-
-        $sentence =~ s{\bcrosslegged\b}{<phoneme alphabet="x-sampa" ph="kr\\OslEgd">crosslegged</phoneme>}gsm;
-        $sentence =~ s{\bKuru\b}{<phoneme alphabet="x-sampa" ph="ku%r\\u">Kuru</phoneme>}gsm;
-        $sentence =~ s{\bWill\b}{<phoneme alphabet="x-sampa" ph="%wil">Will</phoneme>}gsm;
-
-        if ($len <= 1500) {
-            $chunk .= $sentence;
-            $size += $len;
-
-        } else {
-            die "$.: sentence too long: $sentence";
-        }
+    if (!defined pos $_) {
+        process_sentence($_, \$first);
+        next;
     }
 
     if (pos $_ < bytes::length $_) {
-        die "$.: unfinished sentence: ", substr $_, pos $_;
+        process_sentence(substr($_, pos $_), \$first);
+    }
+}
+
+sub process_sentence ($$) {
+    my ($sentence, $rfirst) = @_;
+
+    #warn "found sentence: $sentence";
+    my $len = bytes::length $sentence;
+
+    #warn "size: $size, len: $len";
+
+    if ($size + $len > 1500) {
+        $chunk .= $closing_tags;
+        push @chunks, $chunk;
+        $chunk = $init_tags;
+        $size = 0;
+    }
+
+    if ($$rfirst) {
+        if ($paragraphs > 1) {
+            $chunk .= '<break time="500ms"/>';
+        }
+        $$rfirst = 0;
+    }
+
+    $sentence =~ s/\&/\&amp;/g;
+    $sentence =~ s/</\&lt;/g;
+    $sentence =~ s/>/\&gt;/g;
+    #$sentence =~ s{N`([A-Za-z][^`]*?[A-Za-z])`}{<emphasis level="strong">$1</emphasis>}smg;
+    $sentence =~ s{VB`([A-Za-z][^`]*?[A-Za-z])`}{<w role="amazon:VB">$1</w>}smg;
+
+    $sentence =~ s{(?<!the|and) (["'])(.*?)\1}{
+        my $mark = $1;
+        my $v = $2;
+        if ($v =~ /^\w+$/) {
+            qq! <emphasis>$v</emphasis>!
+        } else {
+            qq! <break time="250ms"/>$mark$v$mark!
+        }
+    }esmg;
+
+    $sentence =~ s{\bcrosslegged\b}{<phoneme alphabet="x-sampa" ph="kr\\OslEgd">crosslegged</phoneme>}gsm;
+    $sentence =~ s{\bKuru\b}{<phoneme alphabet="x-sampa" ph="ku%r\\u">Kuru</phoneme>}gsm;
+    $sentence =~ s{\bWill\b}{<phoneme alphabet="x-sampa" ph="%wil">Will</phoneme>}gsm;
+
+    if ($len <= 1500) {
+        $chunk .= $sentence;
+        $size += $len;
+
+    } else {
+        die "$.: sentence too long: $sentence";
     }
 }
 
